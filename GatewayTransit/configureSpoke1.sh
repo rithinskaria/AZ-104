572309910449bfbc845e361335ec6f16f2830b7d
Rithin Skaria <rithin@rithin.net>
2022-06-04 18:26:30 +0530

    Update configureSpoke1.sh

--- GatewayTransit/configureSpoke1.sh
+++ GatewayTransit/configureSpoke1.sh
@@ -2,7 +2,7 @@
 sudo apt update -y
 sudo apt install apache2 -y
 sudo chown -R -v 777 /var/www/
-echo "<html><body style=\"background-color:red;\"><h1 style=\"color:white;\">Hi from blue-1</h1></body></html>" > /var/www/html/index.html
+echo "<html><body style=\"background-color:red;\"><h1 style=\"color:white;\">Hi from Spoke-1</h1></body></html>" > /var/www/html/index.html
 sudo systemctl start apache2
 sudo systemctl enable apache2
 echo "Configuration done"
