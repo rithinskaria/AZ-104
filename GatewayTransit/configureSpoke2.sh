498e39d59cab932d5fb4fe38eca343eadcbad07f
Rithin Skaria <rithin@rithin.net>
2022-06-04 18:26:18 +0530

    Create configureSpoke2.sh

---
+++ GatewayTransit/configureSpoke2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install apache2 -y
+sudo chown -R -v 777 /var/www/
+echo "<html><body style=\"background-color:blue;\"><h1 style=\"color:white;\">Hi from Spoke-2</h1></body></html>" > /var/www/html/index.html
+sudo systemctl start apache2
+sudo systemctl enable apache2
+echo "Configuration done"
