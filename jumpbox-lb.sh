aa37f111529490e81f2df198bb66a7d712751e3c
Rithin Skaria <rithin@rithin.net>
2022-06-04 06:21:05 +0530

    Update jumpbox-lb.sh

--- jumpbox-lb.sh
+++ jumpbox-lb.sh
@@ -9,7 +9,7 @@ ip="10.0.2.$j"
 sshpass -p "VMP@55w0rd" ssh -o StrictHostKeyChecking=no rithin@$ip bash -c \
 "'sudo apt install apache2 -y
 sudo chmod -R -v 777 /var/www/
-sudo echo "<html><body style="background-color:${colors[$i]};"><h1 style="color:white;">Hello world!</h1></body></html>" > /var/www/html/index.html
+sudo echo "<html><body style=\"background-color:${colors[$i]};\"><h1 style=\"color:white;\">Hello world!</h1></body></html>" > /var/www/html/index.html
 exit
 '"
 done
