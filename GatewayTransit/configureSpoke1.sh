111cabeb243d7bd02b2a8dc94833b171fe410672
Rithin Skaria <rithin@rithin.net>
2022-06-04 18:24:11 +0530

    Create configureSpoke1.sh

---
+++ GatewayTransit/configureSpoke1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install apache2 -y
+sudo chown -R -v 777 /var/www/
+echo "<html><body style=\"background-color:red;\"><h1 style=\"color:white;\">Hi from blue-1</h1></body></html>" > /var/www/html/index.html
+sudo systemctl start apache2
+sudo systemctl enable apache2
+echo "Configuration done"
