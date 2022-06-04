0c5f7a348f1d84b1917e9ef4f592f163c4a40381
Rithin Skaria <rithin@rithin.net>
2022-06-04 06:18:00 +0530

    Update jumpbox-lb.sh

--- jumpbox-lb.sh
+++ jumpbox-lb.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt install sshpass -y
-sshpass -p "VMP@55w0rd" ssh -o StrictHostKeyChecking=no
-rithin@10.0.2.4 <<EOF
-sudo apt install apache2 -y
+colors=("red","green","blue")
+for i in {1..3}
+do
+j=$(($i + 3))
+ip="10.0.2.$j"
+sshpass -p "VMP@55w0rd" ssh -o StrictHostKeyChecking=no rithin@$ip bash -c \
+"'sudo apt install apache2 -y
 sudo chmod -R -v 777 /var/www/
-sudo echo "Hello from web-01" > /var/www/html/index.html
+sudo echo "<html><body style="background-color:${colors[$i]};"><h1 style="color:white;">Hello world!</h1></body></html>" > /var/www/html/index.html
 exit
-EOF
-sshpass -p "VMP@55w0rd" ssh -o StrictHostKeyChecking=no rithin@10.0.2.5 <<EOF
-sudo apt install apache2 -y
-sudo chmod -R -v 777 /var/www/
-sudo echo "Hello from web-02" > /var/www/html/index.html
-exit
-EOF
-sshpass -p "VMP@55w0rd" ssh -o StrictHostKeyChecking=no rithin@10.0.2.6 <<EOF
-sudo apt install apache2 -y
-sudo chmod -R -v 777 /var/www/
-sudo echo "Hello from web-03" > /var/www/html/index.html
-exit
-EOF
+'"
+done
