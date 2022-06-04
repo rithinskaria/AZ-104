419ba40c657e0a5b2d686ed21efdc238d7c8f845
Rithin Skaria <rithin@rithin.net>
2022-06-04 10:40:21 +0530

    Update lab-infra.ps1

--- Azure Load Balancer/lab-infra.ps1
+++ Azure Load Balancer/lab-infra.ps1
@@ -116,7 +116,7 @@ $Params = @{
     Publisher          = 'Microsoft.Azure.Extensions'
     ExtensionType      = 'CustomScript'
     TypeHandlerVersion = '2.1'
-    Settings          = @{fileUris = @('https://raw.githubusercontent.com/rithinskaria/kodekloud-azure/main/jumpbox-lb.sh'); commandToExecute = './jumpbox-lb.sh'}
+    Settings          = @{fileUris = @('https://raw.githubusercontent.com/rithinskaria/kodekloud-azure/main/jumpbox.sh'); commandToExecute = './jumpbox.sh'}
 }
 Set-AzVMExtension @Params
 
